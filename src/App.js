5aa7f62e57b6e8619db2f9dfe4d315352b733e22
彭文聪 <1270527472@qq.com>
2021-06-27 15:35:59 +0800

    项目基本架构搭建完成

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import {connect} from 'react-redux'  //引入连接器
+function App(props) {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+	    {props.inputValue}
       </header>
     </div>
   );
 }
-
-export default App;
+// 建立映射关系
+const stateToProps = (state)=>{
+    return {
+        inputValue : state.inputValue
+    }
+}
+export default connect(stateToProps,null)( App);
---
+++ src/store/reducer.js
@@ -0,0 +1,7 @@
+const defaultState={
+	inputValue:"我是张三"
+}
+const fnc=(state=defaultState,action)=>{
+	return state
+}
+export default fnc;
